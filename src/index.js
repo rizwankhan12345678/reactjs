e986f6e0eb9c82ad2cd79ca0882b2453655b0bea
Rizwan Khan <rizwankhan43917@gmail.com>
2022-11-07 21:28:16 +0530

    Prod deployment setting added

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename='/login'>
       <App />
     </BrowserRouter>
   </Provider>
